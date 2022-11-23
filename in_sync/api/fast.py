c2d4cd49fc2ef930e43da9675c8b091c0d8c5643
Roger Bian <bian14r@gmail.com>
2022-11-23 21:10:05 +0900

    updated api to receive gif directly from streamlit

--- in_sync/api/fast.py
+++ in_sync/api/fast.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from google.cloud import storage
 import cv2 as cv
+from PIL import Image
+import numpy as np
 
 app = FastAPI()
 # app.state.model = load_model()
@@ -22,17 +24,29 @@ def root():
     return {'greeting': 'Hello from in_sync'}
 
 
-@app.get("/vid_process")
-def process(frame_count: int):
-    frames = []
+# @app.get("/vid_process_from_bucket")
+# def process_from_bucket(frame_count: int):
+#     frames = []
     
-    for i in range(1, frame_count+1):
-        storage_client = storage.Client()
-        bucket = storage_client.bucket(BUCKET_NAME)
-        blob = bucket.blob(f'{i}.jpg')
-        blob.download_to_filename(f'{i}.jpg')
+#     for i in range(1, frame_count+1):
+#         storage_client = storage.Client()
+#         bucket = storage_client.bucket(BUCKET_NAME)
+#         blob = bucket.blob(f'{i}.jpg')
+#         blob.download_to_filename(f'{i}.jpg')
         
-        oriImg = cv.imread(f'{i}.jpg')
-        frames.append(oriImg)
+#         oriImg = cv.imread(f'{i}.jpg')
+#         frames.append(oriImg)
         
-    return {'frame_count': len(frames)}
+#     return {'frame_count': len(frames)}
+
+@app.post("/vid_process_from_st")
+def process_from_st(file: UploadFile = File(...)):
+    vid_name = file.filename
+    uploaded_video = file.file
+    with open(vid_name, mode='wb') as f:
+        f.write(uploaded_video.read())
+        
+    vidcap = cv.VideoCapture(vid_name)
+    frame_count = int(vidcap.get(cv.CAP_PROP_FRAME_COUNT))
+    
+    return {'frame_count': frame_count} 
