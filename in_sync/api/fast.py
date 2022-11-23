1cfc044c52f9c50d4d0c6046a6a23fb5bdb6a33c
Roger Bian <bian14r@gmail.com>
2022-11-23 16:29:31 +0900

    updated api to download img from bucket

--- in_sync/api/fast.py
+++ in_sync/api/fast.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .dl_from_bucket import blob_retrieval
+from google.cloud import storage
+import cv2 as cv
 
 app = FastAPI()
 # app.state.model = load_model()
+
+BUCKET_NAME = 'sync_testinput'
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,12 +17,22 @@ app.add_middleware(
 )
 
 
-@app.get("/process")
-def process(file_name: str):
-    contents = blob_retrieval(file_name)
-    return {'blob_contents': str(contents)}
-
-
 @app.get("/")
 def root():
     return {'greeting': 'Hello from in_sync'}
+
+
+@app.get("/vid_process")
+def process(frame_count: int):
+    frames = []
+    
+    for i in range(1, frame_count+1):
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(BUCKET_NAME)
+        blob = bucket.blob(f'{i}.jpg')
+        blob.download_to_filename(f'{i}.jpg')
+        
+        oriImg = cv.imread(f'{i}.jpg')
+        frames.append(oriImg)
+        
+    return {'frame_count': len(frames)}
