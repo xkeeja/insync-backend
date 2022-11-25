679f4f41b6ea1c7c439cad9770d9b98e9b1238be
Roger Bian <bian14r@gmail.com>
2022-11-25 16:41:33 +0900

    streamlit testing updated for api functions

--- streamlit_testing/app.py
+++ streamlit_testing/app.py
@@ -7,7 +7,8 @@ uploaded_video = st.file_uploader("**Upload video for evaluation**", type=(['mp4
 
 #If video has been uploaded
 if uploaded_video:
-    url = "http://127.0.0.1:8000/vid_stats"
+    # url = "http://127.0.0.1:8000/vid_stats"
+    url = "https://syncv4-eagwezifvq-an.a.run.app/vid_stats"
     files = {"file": (uploaded_video.name, uploaded_video, "multipart/form-data")}
     
     start_time = time.time()
@@ -22,15 +23,16 @@ if uploaded_video:
     if post_response:
         if st.button('Analyse synchronization'):
             
-            st.write('Processing...')
-            url = "http://127.0.0.1:8000/vid_processed"
-            params = {"vid_name": post_response['vid_name'], "output_name": post_response['output_name']}
-            
-            start_time = time.time()
-            get_response = requests.get(url, params=params).json()
-            
-            st.write(get_response)
-            st.write("--- %s seconds ---" % (time.time() - start_time))
+            with st.spinner('Processing...'):
+                # url = "http://127.0.0.1:8000/vid_processed"
+                url = "https://syncv4-eagwezifvq-an.a.run.app/vid_processed"
+                params = {"vid_name": post_response['vid_name'], "output_name": post_response['output_name']}
+                
+                start_time = time.time()
+                get_response = requests.get(url, params=params).json()
+                
+                st.write(get_response)
+                st.write("--- %s seconds ---" % (time.time() - start_time))
 
 
-            st.video(get_response['output_url'])
+                st.video(get_response['output_url'])
