7545000ff080cd4fef793bbdf94a4b6bbf16985a
Roger Bian <bian14r@gmail.com>
2022-11-30 00:40:01 +0900

    major revision to handle session state

--- streamlit_testing/app.py
+++ streamlit_testing/app.py
@@ -65,127 +65,162 @@ def processing(stats):
             "dancers": stats['dancers']
             }
         response = requests.get(url, params=params).json()
-
-    #Create df
-    d = {
-        'Time': response['timestamps'],
-        'Sync Error': response['scores']
-    }
-    df = pd.DataFrame(d)
-
-    #Calculate moving average for smoother graph
-    df['Smoothed Sync Error'] = df['Sync Error'].rolling(window=9).mean()
-
-    #Plot moving average graph
-    st.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    st.write('The following graph shows your absolute synchronisation error, calculated as a difference in pose/angle of major joints relative to each other. A lower value is a smaller difference, and therefore better synchronization. Peaks are areas of lowest synchronisation.')
-    # st.line_chart(data=df, x='Time', y='Sync Error')
-    fig1 = go.Figure()
-    fig1.add_trace(go.Scatter(x=df.index, y=list(df['Sync Error']), line=dict(color='#ff008c')))
-    fig1.update_layout(
-            title="Absolute Sync Error Graph",
-            xaxis_title="Frame",
-            yaxis_title="Absolute Sync Error",
-            font=dict(
-                family="Courier New, monospace",
-                size=18,
-                color="RebeccaPurple"
-                ),
-            hovermode='x unified')
-    st.plotly_chart(fig1)
+        return response
 
 
-    st.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    st.write('The following graph shows your average synchronisation error, smoothed for easier reference of intervals needing improvement or attention.')
-    # st.line_chart(data=df, x='Time', y='Smoothed Sync Error')
-    fig2 = go.Figure()
-    fig2.add_trace(go.Scatter(x=df.index, y=list(df['Smoothed Sync Error']), line=dict(color='#ff008c')))
-    fig2.update_layout(
-            title="Smoothed Sync Error Graph",
-            xaxis_title="Frame",
-            yaxis_title="Smoothed Sync Error",
-            font=dict(
-                family="Courier New, monospace",
-                size=18,
-                color="RebeccaPurple"
-                ),
-            hovermode='x unified')
-    st.plotly_chart(fig2)
+#build header
+col1, col2, col3 = st.columns([1,2,1])
+with col1:
+    st_lottie(lottie_dancing, key="dance_left")
+with col2:
+    st.markdown("<h1 style='text-align: center; color: RebeccaPurple;'>in sync.</h1>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center; color: #ff008c;'>your personal AI<br/>synchronisation assistant.</h3>", unsafe_allow_html=True)
+with col3:
+    st_lottie(lottie_dancing, key="dance_right")
 
 
-    #Load processed video
-    video_url = response['output_url']
-    st.video(video_url)
-    
-
-    #Timestamp buttons
-    sorted_df = df.sort_values(by=['Smoothed Sync Error'], ascending=False).round(2).head(5)
-    index = sorted_df.head(5).index
-    top5imp = [i for i in index]
-    timestamps = sorted_df['Time'].to_list()
-    sync = sorted_df['Smoothed Sync Error'].to_list()
-
-    with st.expander("**Your top five areas for improvement:**"):
-        for i in range(len(index)):
-            st.write(f"**Frame {index[i]}**")
-            st.write(f'Timestamp: {timestamps[i]}s, Absolute Sync Error: {sync[i]}')
-            st.image(f"https://storage.googleapis.com/sync_testinput/screencaps/{response['my_uuid']}/frame{top5imp[i]}.jpg")
-            st.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+#Receive video file from user upload
+uploaded_video = st.file_uploader("**upload video for evaluation**", ['mp4'])
 
 
-def main():
-
-    #build header
-    col1, col2, col3 = st.columns([1,2,1])
-    with col1:
-        st_lottie(lottie_dancing, key="dance_left")
-    with col2:
-        st.markdown("<h1 style='text-align: center; color: RebeccaPurple;'>In Sync</h1>", unsafe_allow_html=True)
-        st.markdown("<h3 style='text-align: center; color: #ff008c;'>Dance Synchronisation<br/>Evaluation Tool</h3>", unsafe_allow_html=True)
-    with col3:
-        st_lottie(lottie_dancing, key="dance_right")
-
-
-    #Receive video file from user upload
-    uploaded_video = st.file_uploader("**Upload video for evaluation**", ['mp4'], key='dance')
-    
-    
-    #If video has been uploaded
-    if uploaded_video is not None:
-
-        #Post request with video file
-        with st_lottie_spinner(lottie_model_loading):
-            # url = "https://syncv6-eagwezifvq-an.a.run.app/vid_stats"
-            url = "http://127.0.0.1:8000/vid_stats"
-            files = {"file": (uploaded_video.name, uploaded_video, "multipart/form-data")}
-            stats = requests.post(url, files=files).json()
-
-        #Return uploaded video
-        _, b, _ = st.columns([1,4,1])
-        with b:
-            show_vid = st.video(uploaded_video)
+#If video has been uploaded
+if uploaded_video is not None:
+    # --- Initialising SessionState ---
+    if 'video' not in st.session_state or uploaded_video.name != st.session_state.video:
+        st.session_state.video = uploaded_video.name
+        st.session_state.response = None
         
-        # a, b, c = st.columns(3)
-        # with a:
-        #     display_dial("FPS", f"{stats['fps']}", "#1C83E1")
-        # with b:
-        #     display_dial("FRAMES", f"{stats['frame_count']}", "#1C83E1")
-        # with c:
-        #     display_dial("DIMENSION", f"{stats['dim']}", "#1C83E1")
+    
+    #Post request with video file
+    with st_lottie_spinner(lottie_model_loading):
+        # url = "https://syncv6-eagwezifvq-an.a.run.app/vid_stats"
+        url = "http://127.0.0.1:8000/vid_stats"
+        files = {"file": (uploaded_video.name, uploaded_video, "multipart/form-data")}
+        stats = requests.post(url, files=files).json()
 
-        process_vid = False
+    #Return uploaded video
+    # _, b, _ = st.columns([1,4,1])
+    # with b:
+    show_vid = st.video(uploaded_video)
+    
+    # a, b, c = st.columns(3)
+    # with a:
+    #     display_dial("FPS", f"{stats['fps']}", "#1C83E1")
+    # with b:
+    #     display_dial("FRAMES", f"{stats['frame_count']}", "#1C83E1")
+    # with c:
+    #     display_dial("DIMENSION", f"{stats['dim']}", "#1C83E1")
 
-        _, a, _, b, _ = st.columns([1, 2, 1, 1, 1])
+    process_vid = False
+
+    # _, a, _ = st.columns([3, 2, 3])
+    # with a:
+    dancers = st.number_input("enter number of dancers:", min_value=1, max_value=6)
+    stats['dancers'] = dancers
+    
+    # _, b, _ = st.columns([1, 4, 1])
+    # with b:
+    if st.checkbox("click checkbox to start (RESET this checkbox when new file is uploaded)"):
+        # show_vid.empty()
+
+        st.text('')
+        
+        if st.session_state.response is not None:
+            response = st.session_state.response 
+        else:
+            response = processing(stats)
+            st.session_state.response = response
+        
+        #Empty space
+        st.text('')
+        
+        #Create df
+        d = {
+            'Time': response['timestamps'],
+            'Sync Error': response['scores']
+        }
+        df = pd.DataFrame(d)
+
+
+        #Load processed video
+        video_url = response['output_url']
+        st.video(video_url)
+
+
+        #Plot absolute error graph
+        st.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        st.write('The following graph shows your absolute synchronisation error, calculated as a difference in pose/angle of major joints relative to each other. A lower value is a smaller difference, and therefore better synchronization. Peaks are areas of lowest synchronisation.')
+        # st.line_chart(data=df, x='Time', y='Sync Error')
+        fig1 = go.Figure()
+        fig1.add_trace(go.Scatter(x=df.index, y=list(df['Sync Error']), line=dict(color='#ff008c')))
+        fig1.update_layout(
+                title="Absolute Sync Error Graph",
+                xaxis_title="Frame",
+                yaxis_title="Absolute Sync Error",
+                font=dict(
+                    family="Courier New, monospace",
+                    size=18,
+                    color="RebeccaPurple"
+                    ),
+                hovermode='x unified')
+        st.plotly_chart(fig1)
+
+
+        #Calculate moving average for smoother graph
+        df['Smoothed Sync Error'] = df['Sync Error'].rolling(window=9).mean()
+
+        # Plot smoothed error graph
+        st.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        st.write('The following graph shows your average synchronisation error, smoothed for easier reference of intervals needing improvement or attention.')
+        # st.line_chart(data=df, x='Time', y='Smoothed Sync Error')
+        fig2 = go.Figure()
+        fig2.add_trace(go.Scatter(x=df.index, y=list(df['Smoothed Sync Error']), line=dict(color='#ff008c')))
+        fig2.update_layout(
+                title="Smoothed Sync Error Graph",
+                xaxis_title="Frame",
+                yaxis_title="Smoothed Sync Error",
+                font=dict(
+                    family="Courier New, monospace",
+                    size=18,
+                    color="RebeccaPurple"
+                    ),
+                hovermode='x unified')
+        st.plotly_chart(fig2)
+
+
+        # Plot sliders for frame analysis
+        st.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        frame = st.slider('select a frame to see 6 frames in succession', 0, int(stats['frame_count']), 0)
+        a, b, c = st.columns([1, 1, 1])
         with a:
-            dancers = st.number_input("Enter number of dancers:", min_value=1, max_value=6)
+            st.image(f"https://storage.googleapis.com/sync_testinput/screencaps/{response['my_uuid']}/frame{frame}.jpg")
         with b:
-            if st.button("Start"):
-                show_vid.empty()
-                process_vid = True
+            st.image(f"https://storage.googleapis.com/sync_testinput/screencaps/{response['my_uuid']}/frame{frame+1}.jpg")
+        with c:
+            st.image(f"https://storage.googleapis.com/sync_testinput/screencaps/{response['my_uuid']}/frame{frame+2}.jpg")
+        
+        d, e, f = st.columns([1, 1, 1])
+        with d:
+            st.image(f"https://storage.googleapis.com/sync_testinput/screencaps/{response['my_uuid']}/frame{frame+3}.jpg")
+        with e:
+            st.image(f"https://storage.googleapis.com/sync_testinput/screencaps/{response['my_uuid']}/frame{frame+4}.jpg")
+        with f:
+            st.image(f"https://storage.googleapis.com/sync_testinput/screencaps/{response['my_uuid']}/frame{frame+5}.jpg")
 
-        if process_vid:
-            stats['dancers'] = dancers
-            processing(stats)
 
-if __name__ == '__main__':
-    main()
+        # #Timestamp buttons
+        # sorted_df = df.sort_values(by=['Smoothed Sync Error'], ascending=False).round(2).head(5)
+        # index = sorted_df.head(5).index
+        # top5imp = [i for i in index]
+        # timestamps = sorted_df['Time'].to_list()
+        # sync = sorted_df['Smoothed Sync Error'].to_list()
+
+        # with st.expander("**Your top five areas for improvement:**"):
+        #     for i in range(len(index)):
+        #         st.write(f"**Frame {index[i]}**")
+        #         st.write(f'Timestamp: {timestamps[i]}s, Absolute Sync Error: {sync[i]}')
+        #         st.image(f"https://storage.googleapis.com/sync_testinput/screencaps/{response['my_uuid']}/frame{top5imp[i]}.jpg")
+        #         st.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+# if __name__ == '__main__':
+#     main
