956c08e5aaea7994f2e7acd30cffa3e566879b4c
Roger Bian <bian14r@gmail.com>
2022-12-02 10:22:16 +0900

    conf display

--- api/fast.py
+++ api/fast.py
@@ -66,11 +66,11 @@ def stats_to_st(file: UploadFile = File(...)):
 
 
 @app.get("/vid_process")
-def process_vid(vid_name, output_name, frame_count, fps, width, height, dancers, face_ignored, conf_threshold):
+def process_vid(vid_name, output_name, frame_count, fps, width, height, dancers, face_ignored, conf_threshold, confidence_display):
     vid, writer, _, _, _, _ = load_video_and_release(vid_name, output_format="mp4", output_name=output_name)
 
     #return vid , all_scores, all_people, all_link_mae , worst_link_scores , worst_link_names
-    vid, all_scores, _, _, worst_link_scores, worst_link_names, ignore_frame = predict_on_stream(vid, writer, app.state.model, int(width), int(height), int(dancers), face_ignored, float(conf_threshold), confidence_display=True)
+    vid, all_scores, _, _, worst_link_scores, worst_link_names, ignore_frame = predict_on_stream(vid, writer, app.state.model, int(width), int(height), int(dancers), face_ignored, float(conf_threshold), confidence_display)
     timestamps = np.arange(int(frame_count))/int(fps) #time in seconds
 
     # compress video output to smaller size
