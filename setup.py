ac0ffc97fb99d889d73e268fe1b8e736020423e1
Roger Bian <113004083+xkeeja@users.noreply.github.com>
2022-12-05 18:21:16 +0900

    cleaned package setup

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import find_packages
 from setuptools import setup
 
-#with open('requirements.txt') as f:
-#    content = f.readlines()
-#requirements = [x.strip() for x in content if 'git+' not in x]
+with open('requirements.txt') as f:
+   content = f.readlines()
+requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='in_sync',
-      version="1.2",
-      description="Project Description",
+setup(name='insync',
+      version="1.0",
+      description="Calculate & visualize dance synchronization",
       packages=find_packages(),
-      #install_requires=requirements,
-      test_suite='tests',
+      install_requires=requirements,
+      # test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts'],
+      # scripts=['scripts'],
       zip_safe=False)
